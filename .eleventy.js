80147ccedb91f8929372341aaaa9168328f5a87e
Matt Obee <matt@obee.me>
2024-11-10 22:24:11 +0000

    Remove Sass

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
-const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
+const lightningCSS = require("@11tyrocks/eleventy-plugin-lightningcss");
 const dayjs = require('dayjs')
 const filters = require("./src/_11ty/filters");
 const { EleventyEdgePlugin } = require("@11ty/eleventy");
@@ -6,11 +6,17 @@ const { EleventyEdgePlugin } = require("@11ty/eleventy");
 console.log("hello from .eleventy.js", process.env)
 
 module.exports = (eleventyConfig) => {
+
+  // Copy files
   eleventyConfig.addPassthroughCopy("./src/images");
   eleventyConfig.addPassthroughCopy("./src/favicon");
   eleventyConfig.addPassthroughCopy("./src/scripts");
-  eleventyConfig.addPlugin(eleventySass);
+
+  // Add plugins
+  eleventyConfig.addPlugin(lightningCSS);
   eleventyConfig.addPlugin(EleventyEdgePlugin);
+
+  // Create shortcodes
   eleventyConfig.addShortcode("year", () => `${new dayjs().year()}`);
   eleventyConfig.addShortcode("today", () => `${new dayjs()}`);
 
