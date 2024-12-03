33c307f280a54ed039028471ed1c896672fd791a
Matt Obee <matt@obee.me>
2024-12-03 13:06:54 +0000

    Remove unused meta and timezoneNames data files

--- src/_data/meta.mjs
+++
@@ -1,6 +0,0 @@
-export default {
-  // url: process.env.URL || "http://localhost:4000",
-  siteName: 'Eventua11y',
-  siteDescription: 'A curated list of digital accessibility events.',
-  authorName: 'Matt Obee',
-};
