09ab0f3a6fd854564010e7b51de0c86b0966cee4
Matt Obee <matt@obee.me>
2024-12-04 20:47:17 +0000

    Added Vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,16 @@ import sanity from "@sanity/astro";
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: netlify(),
-  integrations: [
-    sanity({
-      projectId: "2g5zqxo3",
-      dataset: "test",
-      // Set useCdn to false if you're building statically.
-      useCdn: false,
-    }),
-  ],
+  integrations: [sanity({
+    projectId: "2g5zqxo3",
+    dataset: "test",
+    // Set useCdn to false if you're building statically.
+    useCdn: false,
+  }), vue()],
 });
