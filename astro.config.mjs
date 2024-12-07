f08ef17b21ee01984ca2b1b548e39e63a57671e5
Matt Obee <matt@obee.me>
2024-12-07 13:17:10 +0000

    Remove Sanity Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,12 @@
 import { defineConfig } from 'astro/config';
-import sanity from '@sanity/astro';
 import netlify from '@astrojs/netlify';
 import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   adapter: netlify(),
   integrations: [
-    sanity({
-      projectId: process.env.SANITY_PROJECT,
-      dataset: process.env.SANITY_DATASET,
-      // Set useCdn to false if you're building statically.
-      useCdn: process.env.SANITY_CDN,
-    }),
     vue({
       template: {
         compilerOptions: { isCustomElement: (tag) => tag.startsWith('sl-') },
