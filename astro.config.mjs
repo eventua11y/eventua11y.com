4ae4a39e047d4f1f099085af97634adaa84a94ea
Matt Obee <matt@obee.me>
2024-12-14 21:49:18 +0000

    Disable vue dev tools

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
       template: {
         compilerOptions: { isCustomElement: (tag) => tag.startsWith('sl-') },
       },
-      devtools: true,
+      devtools: false,
     }),
   ],
 });
