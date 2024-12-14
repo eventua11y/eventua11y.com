88ecaaff8116f25b7aaa89696c1b9e427cf160ee
Matt Obee <matt@obee.me>
2024-12-14 00:26:17 +0000

    Refactor EventDelivery

--- src/components/EventDelivery.vue
+++ src/components/EventDelivery.vue
@@ -1,8 +1,33 @@
+<script>
+const ATTENDANCE_MODES = {
+  ONLINE: 'online',
+  OFFLINE: 'offline',
+  MIXED: 'mixed'
+};
+</script>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  attendanceMode: {
+    type: String,
+    required: true,
+    validator: (value) => Object.values(ATTENDANCE_MODES).includes(value)
+  },
+  location: {
+    type: String,
+    default: 'International'
+  }
+});
+
+const displayLocation = computed(() => props.location || 'International');
+</script>
+
 <template>
   <div class="event__delivery text-small">
-    <span class="sr-only">Location:</span>
     <span
-      v-if="attendanceMode === 'online'"
+      v-if="attendanceMode === ATTENDANCE_MODES.ONLINE"
       class="event__online"
       itemprop="eventAttendanceMode"
       content="https://schema.org/OnlineEventAttendanceMode"
@@ -10,26 +35,28 @@
       <i class="fa-solid fa-laptop"></i>
       Online
     </span>
+
     <span
-      v-else-if="attendanceMode === 'offline'"
+      v-else-if="attendanceMode === ATTENDANCE_MODES.OFFLINE"
       class="event__location"
       itemprop="eventAttendanceMode"
       content="https://schema.org/OfflineEventAttendanceMode"
     >
       <i class="fa-solid fa-fw fa-location-dot"></i>
       <span itemprop="location" itemscope itemtype="https://schema.org/Place">
-        {{ location || 'International' }}
+        {{ displayLocation }}
       </span>
     </span>
+
     <div
-      v-else-if="attendanceMode === 'mixed'"
+      v-else-if="attendanceMode === ATTENDANCE_MODES.MIXED"
       itemprop="eventAttendanceMode"
       content="https://schema.org/MixedEventAttendanceMode"
     >
       <span class="event__location">
         <i class="fa-solid fa-fw fa-location-dot"></i>
         <span itemprop="location" itemscope itemtype="https://schema.org/Place">
-          {{ location || 'International' }}
+          {{ displayLocation }}
         </span>
       </span>
       <span class="text-muted">and</span>
@@ -40,21 +67,3 @@
     </div>
   </div>
 </template>
-
-<script setup>
-import { defineProps } from 'vue';
-
-const props = defineProps({
-  attendanceMode: {
-    type: String,
-    required: true,
-  },
-  location: {
-    type: String,
-    default: '',
-  },
-});
-</script>
-
-<!-- <style scoped>
-</style> -->
