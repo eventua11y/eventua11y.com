e482652e1867b67f5d8efcd317dfbc1143231938
Matt Obee <matt@obee.me>
2025-01-03 20:49:33 +0000

    Add EventBook component

---
+++ src/components/EventBook.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="book">
+    <span class="text-small text-muted">Accessibility Book Club is reading</span>
+    <a class="book__title" :href="book.link">{{ book.title }}</a>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Book {
+  title: string;
+  link?: string;
+}
+
+defineProps<{
+  book: Book;
+}>();
+</script>
+
+<style>
+.book {
+    display: flex;
+    flex-direction: column;
+    padding: 0 var(--p-space-xs-m);
+}
+</style>
